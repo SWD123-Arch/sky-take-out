3a6233ac7d743810d6479e72a68c8c32f2486add
qiangtu <19881725868@163.com>
2026-08-14 17:54:16 +0800

    员工账号启用禁用业务代码开发

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -80,12 +80,26 @@ public class EmployeeController {
         employeeService.save(employeeDTO);
         return Result.success();
     }
-    @GetMapping
+    @GetMapping("/page")
     @ApiOperation("员工分页查询")
     public Result<PageResult> page(EmployeePageQueryDTO employeePageQueryDTO){
         log.info("员工分页查询,参数为：{}",employeePageQueryDTO);
         PageResult pageResult =  employeeService.pageQuer(employeePageQueryDTO);
         return Result.success(pageResult);
+    }
+
+    /**
+     * 启用禁用员工账号
+     * @param status
+     * @param id
+     * @return
+     */
+    @PostMapping("status/{status}")
+    @ApiOperation("启用禁用员工账号")
+    public Result startOrStop(@PathVariable Integer status,Long id){
+        log.info("启用禁用员工账号:{},{}",status,id );
+        employeeService.startOrStop(status,id);
+        return Result.success();
     }
 
 }
